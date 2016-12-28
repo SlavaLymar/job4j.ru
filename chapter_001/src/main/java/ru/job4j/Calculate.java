34f24d61c2e7b190dc035f00e74c7cd632a8bf3a
SlavaLymar <slavalymar@gmail.com>
2016-12-28 10:30:31 +0300

    maven+junit+hamcrest

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+/**
+* ����� Hello World
+* @author slavalymar
+* @since 28.12.2016
+* @version 1
+* @return Hello World
+*/
+
+public class Calculate{
+	/** main
+	 * @param args
+	 */
+	public static void main(String[] args){
+
+		System.out.println("Hello World");
+	}
+}
