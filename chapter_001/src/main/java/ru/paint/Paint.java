90ace596ef375345efdb519498f70c11d3f9a71e
SlavaLymar <slavalymar@gmail.com>
2017-01-18 16:18:13 +0300

    Strategy pattern task 5.3

---
+++ chapter_001/src/main/java/ru/paint/Paint.java
@@ -0,0 +1,16 @@
+package ru.paint;
+
+
+/**
+ * @author slavalymar
+ * @since 02.01.2017
+ * @version 1
+ */
+public abstract class Paint {
+
+    public abstract String pic();
+
+    public abstract void draw();
+
+}
+
---
+++ chapter_001/src/test/java/ru/paint/PaintTest.java
@@ -0,0 +1,28 @@
+package ru.paint;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author slavalymar
+ * @since 18.01.2017
+ * @version 1
+ */
+public class PaintTest {
+    @Test
+    public void pic() {
+        Paint sq = new Square(3);
+        Paint tr = new Triangle(3);
+
+        String expected1 = sq.pic();
+        String expected2 = tr.pic();
+
+        String result1 = ".  .  .  \n.  .  .  \n.  .  .  \n";
+        String result2 = "  . \n . . \n. . . \n";
+
+        assertThat(result1, is(expected1));
+        assertThat(result2, is(expected2));
+    }
+
+}
