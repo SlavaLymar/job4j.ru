43eac57b533d6cea3060fe0095726b02084c29c4
SlavaLymar <slavalymar@gmail.com>
2017-01-18 16:12:37 +0300

    strategy pattern (reverted from commit 980c006ec64120bbcc3944928ae52c7b2624844d)

--- chapter_001/src/main/java/ru/paint/Paint.java
+++ chapter_001/src/main/java/ru/paint/Paint.java
@@ -6,10 +6,29 @@ package ru.paint;
  * @since 02.01.2017
  * @version 1
  */
-public abstract class Paint {
+public class Paint {
 
-    public abstract String pic();
+    /**
+     * @param h
+     * @return sb.toString()
+     */
+    String pyramid(int h){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<h; i++){
+            for(int j = 0; j<(h-i)-1; j++){
+                sb.append(" ");
+            }
+            for(int k = 0; k<=i; k++){
+                sb.append("^ ");
+            }
+            sb.append("\n");
+        }
 
-    public abstract void draw();
+        return sb.toString();
+    }
 
+    public static void main(String[] args) {
+        Paint p = new Paint();
+        System.out.println(p.pyramid(3));
+    }
 }
