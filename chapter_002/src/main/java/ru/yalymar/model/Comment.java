aa463a10420d69b507e9f5068e02eb86479bf275
SlavaLymar <slavalymar@gmail.com>
2017-01-21 19:01:14 +0300

    add chapter_002

---
+++ chapter_002/src/main/java/ru/yalymar/model/Comment.java
@@ -0,0 +1,22 @@
+package ru.yalymar.model;
+
+/**
+ * @author slavalymar
+ * @since 13.01.2017
+ * @version 1
+ */
+public class Comment {
+
+    private String comment;
+
+    public Comment(String comment) {
+
+        this.comment = comment;
+    }
+
+    public String getComment() {
+
+        return comment;
+    }
+
+}
