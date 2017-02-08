589ae7cd4e682e97263237e9607bf9c9504da6dc
SlavaLymar <slavalymar@gmail.com>
2017-02-08 17:05:12 +0300

    add test for "hello

--- chapter_003/src/test/java/ru/yalymar/botTheOralce/ServerTest.java
+++ chapter_003/src/test/java/ru/yalymar/botTheOralce/ServerTest.java
@@ -11,7 +11,7 @@ import static org.mockito.Mockito.when;
 public class ServerTest {
 
     @Test
-    public void inputTest() throws IOException {
+    public void inputTestExit() throws IOException {
         Socket socket = mock(Socket.class);
         Server server = new Server(socket);
         ByteArrayInputStream in = new ByteArrayInputStream("exit".getBytes());
@@ -21,6 +21,23 @@ public class ServerTest {
         server.startServer();
         assertThat(out.toString(),
                 is(String.format("%s%s", "Oracle: Bye bye. See you later.",
+                        System.getProperty("line.separator"))));
+    }
+
+    @Test
+    public void inputTestHello() throws IOException {
+        Socket socket = mock(Socket.class);
+        Server server = new Server(socket);
+        ByteArrayInputStream in = new ByteArrayInputStream(
+                String.format("%s%s%s", "hello", System.getProperty("line.separator"),
+                        "exit").getBytes());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        when(socket.getInputStream()).thenReturn(in);
+        when(socket.getOutputStream()).thenReturn(out);
+        server.startServer();
+        assertThat(out.toString(),
+                is(String.format("%s%s%s%s", "Oracle: Hello, dear friend, I'm a oracle.",
+                        System.getProperty("line.separator"), "Oracle: Bye bye. See you later.",
                         System.getProperty("line.separator"))));
     }
 
