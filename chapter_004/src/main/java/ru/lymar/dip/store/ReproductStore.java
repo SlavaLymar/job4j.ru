4c86acc14160d9df767477328f47a73af436c861
SlavaLymar <slavalymar@gmail.com>
2017-02-28 22:14:02 +0300

    remove

---
+++ chapter_004/src/main/java/ru/lymar/dip/store/ReproductStore.java
@@ -0,0 +1,37 @@
+package ru.lymar.dip.store;
+
+import ru.lymar.dip.food.ReproductFood;
+import ru.lymar.dip.storedecorator.Reproduct;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author slavalymar
+ * @since 19.02.2017
+ * @version 1
+ */
+public class ReproductStore implements Reproduct {
+
+    private List <ReproductFood> reproductStore = new ArrayList<>();
+
+    /** if food is reproduct add to reproductStore
+     * @param food
+     * @return boolean
+     */
+    @Override
+    public boolean add(ReproductFood food) {
+        if (food.getCorruptionPercent() >= 100) {
+            this.reproductStore.add(food);
+            return true;
+        }
+        return false;
+    }
+
+    /** getter
+     * @return List
+     */
+    @Override
+    public List<ReproductFood> getList() {
+        return this.reproductStore;
+    }
+}
