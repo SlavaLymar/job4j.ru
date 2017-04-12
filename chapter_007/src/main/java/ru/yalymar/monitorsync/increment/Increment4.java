f85229d0dd362c42cb5521c2d6b50334f5fbdeda
Slava Lymar <slavalymar@gmail.com>
2017-04-12 15:26:32 +0300

    correct task 7.3.1

---
+++ chapter_007/src/main/java/ru/yalymar/monitorsync/increment/Increment4.java
@@ -0,0 +1,72 @@
+package ru.yalymar.monitorsync.increment;
+
+/**
+ * @author slavalymar
+ * @since 04.04.2017
+ * @version 1
+ */
+public class Increment4 {
+
+    /**
+     * those what we want to increment
+     */
+    private int i = 0;
+
+    public int getI() {
+        return this.i;
+    }
+
+    // Class that is thread
+    private class IncrementI extends Thread {
+
+        int tmp;
+
+        IncrementI(int initTmp){
+            this.tmp = initTmp;
+        }
+
+        /**
+         * increment tmp
+         */
+        private void add() {
+            System.out.println(this.getName());
+            this.tmp++;
+        }
+
+        /**
+         * run thread
+         */
+        @Override
+        public void run() {
+            for(int i = 0; i<10000; i++) {
+                this.add();
+            }
+
+            // working :) 
+            i = i + this.tmp;
+        }
+    }
+
+    /** main method
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // initialized
+        Increment4 increment = new Increment4();
+        IncrementI thread1 = increment.new IncrementI(increment.getI());
+        IncrementI thread2 = increment.new IncrementI(increment.getI());
+
+        // start threads
+        thread1.start();
+        thread2.start();
+
+        // wait finishing threads
+        thread1.join();
+        thread2.join();
+
+        // print result
+        System.out.println(String.format("Value i: %d", increment.i));
+
+    }
+}
