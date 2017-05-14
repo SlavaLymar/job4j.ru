6464e34977f0674821f414498c4b58588b2c2a7e
Slava Lymar <slavalymar@gmail.com>
2017-05-14 15:39:53 +0300

    start chapter_009

---
+++ chapter_009/src/main/java/ru/yalymar/webstructure/EchoServlet.java
@@ -0,0 +1,22 @@
+package ru.yalymar.webstructure;
+
+import org.apache.log4j.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class EchoServlet extends HttpServlet{
+
+    private static final Logger logger = Logger.getLogger(EchoServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("hello world!");
+        writer.flush();
+    }
+}
