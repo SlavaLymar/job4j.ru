3e4b89948dd0b2854b6400ba3d8f938f50d62bb0
SlavaLymar <slavalymar@gmail.com>
2017-08-18 16:36:19 +0300

    task 11.2

--- examples/src/main/java/ru/yalymar/proverka/Paint.java
+++ examples/src/main/java/ru/yalymar/proverka/Paint.java
@@ -1,13 +1,8 @@
 package ru.yalymar.proverka;
 
-import java.util.List;
-
 public class Paint {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-		System.out.println(10 >> 1);
 	}
-
-
 }
